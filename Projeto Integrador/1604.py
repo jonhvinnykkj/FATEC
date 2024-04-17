d37a7ef25742115ae6a1ec104405cbd07f82a252
bolsolula22 <162506760+jonhvinnykkj@users.noreply.github.com>
2024-04-16 22:01:14 -0300

    Refactor code to retrieve and print a list of games on sale from the Xbox website

--- Projeto Integrador/1604.py
+++ Projeto Integrador/1604.py
@@ -1,35 +1,49 @@
-import requests
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
 
-# Sua chave de API da Steam
-API_KEY = '31B85073CC7BE22126DE693E7AD9EC2B'
+# Inicialize o driver do navegador (você precisa do driver correto para o seu navegador instalado, neste exemplo, usamos o Chrome)
+driver = webdriver.Chrome()
 
-# URL base da Steam Web API para obter informações sobre jogos em promoção
-url = 'http://api.steampowered.com/ISteamApps/GetAppList/v2/'
+# Carregue a página
+driver.get('https://www.xbox.com/pt-br/promotions/sales/sales-and-specials?xr=shellnav')
 
-# Fazendo a solicitação GET
-response = requests.get(url)
-# Verificando se a solicitação foi bem-sucedida
-if response.status_code == 200:
-    # Convertendo a resposta para JSON
-    data = response.json()
-    
-    # Extraindo a lista de aplicativos (jogos)
-    apps = data['applist']['apps']
+# Aguarde o JavaScript ser executado
+time.sleep(15)
 
-    #coletanto os jogos que estão em promoção
-    jogos_promocao = []
-    for app in apps:
-        id = app['appid']
-        url = f'http://store.steampowered.com/api/appdetails?appids={id}&cc=br&l=pt'
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            if data[str(id)]['success']:
-                jogo = data[str(id)]['data']
-                if 'price_overview' in jogo:
-                    preco = jogo['price_overview']
-                    if preco['discount_percent'] > 0:
-                        jogos_promocao.append(jogo['name'])
-    print(jogos_promocao)
-else:
-    print('Falha na solicitação')
+# Obtenha o conteúdo da página
+html = driver.page_source
+
+# Use BeautifulSoup para analisar o conteúdo
+soup = BeautifulSoup(html, 'html.parser')
+
+# Agora você pode encontrar os elementos como antes
+games = soup.find_all('div', class_='m-panes-product-placement-item')
+
+catalog = []
+for game in games:
+    title_div = game.find('h3', class_='c-heading-4')
+    price_div = game.find('div', class_='c-price')
+
+    if title_div and price_div:
+        title = title_div.text
+        original_price_div = price_div.find('s')
+        if original_price_div:
+            original_price = original_price_div.text
+        else:
+            original_price = "Original price not found"
+        current_price = price_div.text
+        catalog.append([title, original_price, current_price])
+    else:
+        print("Title or price not found for a game")
+
+# Armazene os dados em um arquivo CSV
+import csv
+
+with open('xbox_sales.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Title", "Original Price", "Current Price"])
+    for game in catalog:
+        writer.writerow(game)
+
+        
